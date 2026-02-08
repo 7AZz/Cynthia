cdd2f3b411c6e7f13ddb688ba44dfce96a4123ce
Tanveer Alam <ansaritaz78601@gmail.com>
2026-02-08 21:03:02 +0530

    added the animation for the mouse pointer

--- script.js
+++ script.js
@@ -3,38 +3,64 @@ const scroll = new LocomotiveScroll({
   smooth: true,
 });
 
-function firstPageAnim(){
-    var tl = gsap.timeline();
+function firstPageAnim() {
+  var tl = gsap.timeline();
 
-    tl.from("#nav",{
-        y: '-10',
-        opacity: 0,
-        duration: 1.5,
-        ease: Expo.easeInOut,
+  tl.from("#nav", {
+    y: "-10",
+    opacity: 0,
+    duration: 1.5,
+    ease: Expo.easeInOut,
+  })
+
+    .to(".boundingelem", {
+      y: "0",
+      duration: 2,
+      ease: Expo.easeInOut,
+      delay: -1,
+      stagger: 0.2,
     })
 
-    .to(".boundingelem",{
-        y: '0',
-        duration: 2,
-        ease: Expo.easeInOut,
-        delay: -1,
-        stagger: .2
-    })
-
-    .from("#herofooter",{
-        y: '-10',
-        opacity: 0,
-        duration: 1.5,
-        delay: -1,
-        ease: Expo.easeInOut,
-    })
+    .from("#herofooter", {
+      y: "-10",
+      opacity: 0,
+      duration: 1.5,
+      delay: -1,
+      ease: Expo.easeInOut,
+    });
 }
 
-function circlemousefollower(){
-    window.addEventListener("mousemove", (function (dets){
-        document.querySelector("#minicircle").style.transform = `translate(${dets.clientX}px, ${dets.clientY}px)`;
-    }))
+var timeout ;
+function circlemouseanim() {
+  var xscale = 1;
+  var yscale = 1;
+  var xprev = 0;
+  var yprev = 0;
+  window.addEventListener("mousemove", function (dets) {
+    clearTimeout(timeout);
+
+    xscale = gsap.utils.clamp(0.8, 1.2, dets.clientX - xprev);
+    yscale = gsap.utils.clamp(0.8, 1.2, dets.clientY - yprev);
+
+    xprev = dets.clientX;
+    yprev = dets.clientY;
+
+    circlemousefollower(xscale, yscale);
+
+     timeout = setTimeout(function(){
+        document.querySelector("#minicircle").style.transform =
+      `translate(${dets.clientX}px, ${dets.clientY}px) scale(1, 1)`; // Reset to original size after a short delay
+    }, 100);
+  });
+}
+
+function circlemousefollower(xscale, yscale) {
+  window.addEventListener("mousemove", function (dets) {
+    document.querySelector("#minicircle").style.transform =
+      `translate(${dets.clientX}px, ${dets.clientY}px) scale(${xscale}, ${yscale})`; // Apply both translation and scaling
+  });
 }
 
 firstPageAnim();
 circlemousefollower();
+circlemouseanim();
